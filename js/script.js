396f33f33817947cfbfd9d1fa451e817ebfc86b2
msedkiewicz <magdalenainspirations@gmail.com>
2024-02-09 20:10:05 +0100

    Fix menu display; add removal of sticky class

--- js/script.js
+++ js/script.js
@@ -49,13 +49,19 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const observer = new IntersectionObserver(
   (entries) => {
     const ent = entries[0];
+
     if (ent.isIntersecting === false) {
       document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
     }
   },
   {
     root: null,
     threshold: 0,
+    rootMargin: "-80px",
   }
 );
 observer.observe(sectionHeroEl);
